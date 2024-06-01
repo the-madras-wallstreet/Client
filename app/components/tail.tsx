a335ad82dc29c8db4a4c509993e5df68a3431554
samderzero.official <garages@dehustle.institute>
2024-06-01 18:01:07 +0530

    feat(tail):add dynamic tail component

--- app/components/tail.tsx
+++ app/components/tail.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link"
 import styles from "../styles/tail.module.css"
+import Image from "next/image"
 
 interface TailProps {
     ctrlLinks? : {
         label: string,
-        url: string
+        url: string,
+        icon: string
     }[]
 }
 
@@ -12,13 +14,21 @@ const Tail: React.FC<TailProps> = (props) => {
 
     const {ctrlLinks} = props
 
+    const active_status = 'inactive'
+
 
     const renderLinks = ctrlLinks?.map((link:any,i:any)=>{
-        return (<Link  style={{
-            color: 'white',
-            listStyle:'none', textDecoration: 'none', placeSelf: 'center'}} key={i} href={link.url}>
+        return (
+        <div className={styles.control} key={i}>
+              <Image src={`/icons/${active_status}/${link.icon}.svg`} alt="alt" width={24} height={24} />
+        <Link  style={{
+            color: 'var(--inactive)',fontSize: '10px',
+            listStyle:'none', textDecoration: 'none'}}  href={link.url}>
             {link.label}
-        </Link>)
+        </Link>
+        </div>
+                      
+        )
     })
 
     return(
