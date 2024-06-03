390f5fd9aab1ded86a3041b3c76b9599ac844748
samderzero.official <garages@dehustle.institute>
2024-06-03 23:16:30 +0530

    feat(maintenance):Added maintenance message

--- app/(lander)/estimations/page.tsx
+++ app/(lander)/estimations/page.tsx
@@ -1,5 +1,10 @@
+import styles from "../../styles/tail.module.css";
+
 const Page = () => {
-    return<p>Estimate the cost of scaling at its ease with our estimate calculator</p>
+    return <div className={styles.estimate}>
+        <p>Estimate the cost of scaling at its ease with our estimate calculator</p>
+        <h4>Website is under Maintenance</h4>
+    </div>
 }
 
 export default Page
--- app/(lander)/works/page.tsx
+++ app/(lander)/works/page.tsx
@@ -1,5 +1,8 @@
+import styles from "../../styles/tail.module.css";
+
 const Page = () => {
-    return<p>We are outcome focused and result driven. Here is our recent achievements</p>
+    return <div className={styles.works}> <p>We are outcome focused and result driven. Here is our recent achievements</p>
+     <h4>Website is under Maintenance</h4></div>
 }
 
 export default Page
