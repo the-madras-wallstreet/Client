2d6e9c5fd9c22be377617d701041c70457293672
samderzero.official <garages@dehustle.institute>
2024-06-03 22:26:56 +0530

    feat(added link):Added audit form

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,19 @@ import "./globals.css";
 
 export default function Home() {
 
+  const formUrl = "https://forms.gle/NBuyAfbNV1wCqGiA6";  
+
+  const formHandler = () => {
+    window.open(formUrl, '_blank');  
+  };
+// const formHandler = () => {
+//          window.location.href = "https://forms.gle/NBuyAfbNV1wCqGiA6"
+        
+//     }
 
   const renderContent = <div className={styles.content}>
+
+    
 
     <div>
 
@@ -21,7 +32,7 @@ export default function Home() {
       <p>We’re data-driven strategists delivering growth optimization approaches to unlock the full swing potential of revenue streams in your business.</p>
 
     </div>
-    <button style={{
+    <button onClick={formHandler} style={{
       backgroundColor: '#00D622',
       boxShadow: '0.684px 2.737px 32.85px 0px rgba(151, 255, 168, 0.53)',
       margin: '6px 12px'
